b41079118488c9cf0953be0da4d6f1ebcf9cb3b3
RoundFaceJ <junm21@outlook.com>
2025-06-15 20:09:29 +0800

    Delete mmdetection/mmdetection directory

--- mmdetection/mmdetection/my_configs/faster_rcnn/faster-rcnn_r101_fpn_100_omni.py
+++
@@ -1,8 +0,0 @@
-_base_ = './faster-rcnn_r50_fpn_100_omni.py'
-model = dict(
-    backbone=dict(
-        depth=101,
-        init_cfg=dict(type='Pretrained',
-                      checkpoint='torchvision://resnet101')))
-load_from = "/mnt/nvme0n1/xpj_files/project/OMNI/mmdetection/checkpoints/faster_rcnn/faster_rcnn_r101_fpn_mstrain_3x_coco_20210524_110822-4d4d2ca8.pth"
-work_dir = '/mnt/nvme0n1/xpj_files/project/OMNI/mmdetection/work_dirs3/faster-rcnn/faster-rcnn_r101_fpn_100_omni'
--- mmdetection/mmdetection/my_configs/mask_rcnn/mask-rcnn_r101_fpn_100_omni.py
+++
@@ -1,8 +0,0 @@
-_base_ = './mask-rcnn_r50_fpn_100_omni.py'
-model = dict(
-    backbone=dict(
-        depth=101,
-        init_cfg=dict(type='Pretrained',
-                      checkpoint='torchvision://resnet101')))
-work_dir = '/mnt/nvme0n1/xpj_files/project/OMNI/mmdetection/work_dirs3/mask-rcnn/mask-rcnn_r101_fpn_100_omni'
-load_from = "/mnt/nvme0n1/xpj_files/project/OMNI/mmdetection/checkpoints/mask_rcnn/mask_rcnn_r101_fpn_mstrain-poly_3x_coco_20210524_200244-5675c317.pth"
